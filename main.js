6e6443d8d3009bba85f317dc0b9976a184f3d0f8
Dimon <Dimon999>
2024-02-26 16:31:58 +0300

    Adding a solution

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("yes")
+function checkLengthElem(array) {
+    let result = [];
+    for(let i = 0; i < array.length; i++) {
+        if(array[i].length <= 3) {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
+}
+
+console.log(checkLengthElem(['Hello', '2', 'world', ':-)']));
+console.log(checkLengthElem(['1234', '1567', '-2', 'computer science']));
+console.log(checkLengthElem(['Russia', 'Denmark', 'Kazan']));
